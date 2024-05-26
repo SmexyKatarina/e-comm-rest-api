18937a69f26f12f154487d35dc244aa9261b0906
Kat <mercier200012@hotmail.com>
2024-05-26 13:43:06 -0400

    Update to Footer on project name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <>
             <div className="footer">
-                <p className="footer-project">E-Comm --- By <a href="https://github.com/smexykatarina" target="_blank" rel="noreferrer">Titus</a></p>
+                <p className="footer-project">E-Comm with REST API --- By <a href="https://github.com/smexykatarina" target="_blank" rel="noreferrer">Titus</a></p>
                 <a className="footer-github" href="https://github.com/SmexyKatarina/e-comm-rest-api" target="_blank" rel="noreferrer">Want to see the code?</a>
             </div>
         </>
